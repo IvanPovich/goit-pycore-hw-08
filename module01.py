bc0890e24f9910db55d371a9c665fbe7a5cab972
Ivan_Enko <110199ws@gmail.com>
2024-07-29 13:33:12 +0300

    first commit

---
+++ module01.py
@@ -0,0 +1,86 @@
+from collections import UserDict
+from datetime import datetime
+import re
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if self._is_valid(value):
+            super().__init__(value)
+        else:
+            raise ValueError("Не вірний формат телефону. Повинно бути 10 цифр")
+
+    def _is_valid(self, value):
+        return re.fullmatch(r'\d{10}', value) is not None
+
+class Birthday(Field):
+    def __init__(self, value):
+        try:
+            self.value = datetime.strptime(value, "%d.%m.%Y")
+        except ValueError:
+            raise ValueError("Неправильний формат дати. Ось потрібний формат DD.MM.YYYY")
+
+    def __str__(self):
+        return self.value.strftime("%d.%m.%Y")
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for i, phone in enumerate(self.phones):
+            if phone.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+                break
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def add_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
+
+    def __str__(self):
+        phones_str = '; '.join(p.value for p in self.phones)
+        birthday_str = str(self.birthday) if self.birthday else "не вказано"
+        return f"Contact name: {self.name.value}, phones: {phones_str}, birthday: {birthday_str}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+    def get_upcoming_birthdays(self, days=7):
+        today = datetime.today()
+        upcoming_birthdays = []
+        for record in self.data.values():
+            if record.birthday:
+                next_birthday = record.birthday.value.replace(year=today.year)
+                if today <= next_birthday <= today + timedelta(days=days):
+                    upcoming_birthdays.append(record)
+        return upcoming_birthdays
